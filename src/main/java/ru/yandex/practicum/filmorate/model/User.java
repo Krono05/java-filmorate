34547c21c4249233674d5c6d828130e6e8bf29e1
Тамир Болданов <rpp-2011@yandex.ru>
2023-10-03 12:01:48 +0300

    init fz9

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,23 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class User {
     private int id;
-    @Email(message = "Почта не валидна", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @Email
+    @NotBlank
     private String email;
-    @NotBlank(message = "Логин не может быть пустым")
-    @Pattern(regexp = "^\\S*$", message = "В логине не может быть пробелов")
+    @NotBlank
     private String login;
-
     private String name;
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    @Past
     private LocalDate birthday;
+
+     public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
