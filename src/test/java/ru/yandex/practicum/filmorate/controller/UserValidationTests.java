e7d10ef3e16ea45f9160c6d7c79df16adc456c9d
Тамир Болданов <rpp-2011@yandex.ru>
2023-10-11 12:26:30 +0300

    add_friends_likes

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserValidationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserValidationTests.java
@@ -8,9 +8,10 @@ import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserValidationTests {
 
@@ -26,28 +27,42 @@ public class UserValidationTests {
     @Test
     public void shouldNotValidateIfNullEmail() {
         User nullEmailUser = new User(null, "Тамир3000", "Тамир", LocalDate.of(1988, 8, 29));
-        assertThrows(ValidationException.class, () -> { userController.createUser(nullEmailUser); });
+        assertThrows(ValidationException.class, () -> {
+            userController.createUser(nullEmailUser);
+        });
         User noEmailUser = new User("", "Тамир3000", "Тамир", LocalDate.of(1988, 8, 29));
-        assertThrows(ValidationException.class, () -> { userController.createUser(noEmailUser); });
+        assertThrows(ValidationException.class, () -> {
+            userController.createUser(noEmailUser);
+        });
         User gapEmailUser = new User(" ", "Тамир3000", "Тамир", LocalDate.of(1988, 8, 29));
-        assertThrows(ValidationException.class, () -> { userController.createUser(gapEmailUser); });
+        assertThrows(ValidationException.class, () -> {
+            userController.createUser(gapEmailUser);
+        });
     }
 
     @Test
     public void shouldNotValidateIfNoAt() {
         User wrongEmailUser = new User("google.com", "Тамир3000", "Тамир", LocalDate.of(1988, 8, 29));
-        ValidationException thrown = assertThrows(ValidationException.class, () -> {userController.createUser(wrongEmailUser); });
+        ValidationException thrown = assertThrows(ValidationException.class, () -> {
+            userController.createUser(wrongEmailUser);
+        });
         assertEquals("Адрес электронной почты не может быть пустым и должен содержать символ @.", thrown.getMessage());
     }
 
     @Test
     public void shouldNotValidateIfLoginNull() {
         User nullLoginUser = new User("test@test.ru", null, "Тамир", LocalDate.of(1988, 8, 29));
-        assertThrows(ValidationException.class, () -> { userController.createUser(nullLoginUser); });
+        assertThrows(ValidationException.class, () -> {
+            userController.createUser(nullLoginUser);
+        });
         User noLoginUser = new User("test@test.ru", "", "Тамир", LocalDate.of(1998, 8, 29));
-        assertThrows(ValidationException.class, () -> { userController.createUser(noLoginUser); });
+        assertThrows(ValidationException.class, () -> {
+            userController.createUser(noLoginUser);
+        });
         User gapLoginUser = new User("test@test.ru", " ", "Тамир", LocalDate.of(1998, 8, 29));
-        ValidationException thrown = assertThrows(ValidationException.class, () -> { userController.createUser(gapLoginUser); });
+        ValidationException thrown = assertThrows(ValidationException.class, () -> {
+            userController.createUser(gapLoginUser);
+        });
         assertEquals("Логин не должен быть пустым и не должен содержать пробелы.", thrown.getMessage());
     }
 
@@ -61,7 +76,9 @@ public class UserValidationTests {
     @Test
     public void shouldNotValidateIfBirthdayInFuture() {
         User user = new User("test@test.ru", "Тамир3000", "Тамир", LocalDate.of(2024, 3, 29));
-        ValidationException thrown = assertThrows(ValidationException.class, () -> {userController.createUser(user); });
+        ValidationException thrown = assertThrows(ValidationException.class, () -> {
+            userController.createUser(user);
+        });
         assertEquals("Дата рождения не может быть в будущем.", thrown.getMessage());
     }
 }
