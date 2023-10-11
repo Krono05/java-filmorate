c2ca425185888ea6fd64e65a6ef17f28b514bfc2
Тамир Болданов <rpp-2011@yandex.ru>
2023-10-11 12:12:18 +0300

    add_friends_likes

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -16,7 +16,8 @@ public class UserService {
 
     private final UserStorage userStorage;
     int random = 0;
-    
+
+
     public UserService(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
